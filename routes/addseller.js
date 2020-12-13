501caff496d9ebf1212e1de2c7a0d760b69c0d10
rukhshan23 <74811379+rukhshan23@users.noreply.github.com>
2020-12-14 03:46:59 +0500

    zain

--- routes/addseller.js
+++ routes/addseller.js
@@ -3,14 +3,13 @@ exports.display= async function(req,res){
 	let name=req.body.name
 	let subs_id=req.body.subs_id
 	let phone=req.body.phone
-	let discount=req.body.discount
 	let account_id=req.body.account_id
 	console.log(name);
 	console.log(subs_id);
 	console.log(phone);
 	console.log(discount);
 	console.log(account_id);
-	connection.query("INSERT INTO seller(name,subs_id,phone,discount,account_id) values(?,?,?,?,?)",[name,subs_id,phone,discount,account_id] ,async function (error,results,fields){
+	connection.query("INSERT INTO seller(name,subs_id,phone,account_id) values(?,?,?,?)",[name,subs_id,phone,account_id] ,async function (error,results,fields){
 	if (error){
 		res.send({
 			'code':404,
