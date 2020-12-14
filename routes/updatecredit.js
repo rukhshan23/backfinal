32c1554df9f9e9e4356ef6e36c3f251faeed5288
rukhshan23 <74811379+rukhshan23@users.noreply.github.com>
2020-12-14 22:49:24 +0500

    cred

--- routes/updatecredit.js
+++ routes/updatecredit.js
@@ -2,8 +2,9 @@ const connection=require("../database");
 exports.display= async function(req,res){
   
     let buyer_id=req.body.buyer_id
+    let bill=req.body.bill
 
-    connection.query("update buyer set credit=credit+100 where buyer_id=?",[buyer_id] ,async function (error,results,fields){
+    connection.query("update buyer set credit=credit+? where buyer_id=?",[bill*0.01,buyer_id] ,async function (error,results,fields){
         if (error){
             res.send({
                 'code':404,
